41a190b5586f9fc90330c5b2bce9630053295732
Matthew Williams & sterling houghton <student@epicodus.com>
2014-08-26 15:23:18 -0700

    add complete CRUD methods for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -11,4 +11,29 @@ class LessonsController < ApplicationController
     @lesson = Lesson.create(params[:lesson])
     render('/lessons/index.html.erb')
   end
+
+  def delete
+    @lessons = Lesson.all
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    render('/lessons/index.html.erb')
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+    render('/lessons/show.html.erb')
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+    render('/lessons/edit.html.erb')
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    @lesson.update(:content => params[:content],
+                   :name => params[:name],
+                   :number => params[:number])
+    render('/lessons/show.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'lessons#index'})
   match('/', {:via => :post, :to => 'lessons#create'})
+  match('/:id', {:via => :delete, :to => 'lessons#delete'})
+  match('/show/:id', {:via => :get, :to => 'lessons#show'})
+  match('/edit/:id', {:via => :get, :to => 'lessons#edit'})
+  match('/:id', {:via => [:patch, :put], :to => 'lessons#update'})
 end
